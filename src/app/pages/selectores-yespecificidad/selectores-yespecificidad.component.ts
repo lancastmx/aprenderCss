630884698476ee0e36651d1d8468f7aee2ed1a93
lancast <angelprep92@gmail.com>
2025-12-01 10:02:44 -0600

    feat:Primera pagina

--- src/app/pages/selectores-yespecificidad/selectores-yespecificidad.component.ts
+++ src/app/pages/selectores-yespecificidad/selectores-yespecificidad.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-selectores-yespecificidad',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './selectores-yespecificidad.component.html',
   styleUrl: './selectores-yespecificidad.component.css'
 })
 export class SelectoresYespecificidadComponent {
-
+ a = 1;
+ nombre = 'Juan';
+ listanombres= ['Ana', 'Luis', 'Carlos', 'María', 'Jorge'];
 }
